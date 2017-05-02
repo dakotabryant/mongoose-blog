052861c94b8bcd09008eab79c22cd3a120865e12
Dakota Bryant <bryant.dakota@gmail.com>
2017-05-02 14:20:44 -0500

    finished second get endpoint

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// mongo ds129281.mlab.com:29281/blog-api -u dev -p donald
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
@@ -5,8 +6,13 @@ const morgan = require('morgan');
 
 mongoose.Promise = global.Promise;
 
-const {DATABASE_URL, PORT} = require('./config');
-const {Post} = require('./models');
+const {
+	DATABASE_URL,
+	PORT
+} = require('./config');
+const {
+	Post
+} = require('./models');
 
 
 const app = express();
@@ -14,58 +20,68 @@ app.use(bodyParser.json());
 
 
 app.get('/posts', (req, res) => {
-  Post
-    .find()
-    .exec()
-    .then(posts => {
-      res.json(posts);
-    });
-
-    
+	Post
+		.find()
+		.exec()
+		.then(posts => {
+			res.json(posts);
+		});
+});
+app.get('/posts/:id', (req, res) => {
+	Post
+		.findById(req.params.id)
+		.exec()
+		.then(post => {
+			res.json(post);
+		})
 });
 
 
 
 let server;
 
-function runServer(databaseUrl=DATABASE_URL, port=PORT) {
+function runServer(databaseUrl = DATABASE_URL, port = PORT) {
 
-  return new Promise((resolve, reject) => {
-    mongoose.connect(databaseUrl, err => {
-      if (err) {
-        return reject(err);
-      }
-      server = app.listen(port, () => {
-        console.log(`Your app is listening on port ${port}`);
-        resolve();
-      })
-      .on('error', err => {
-        mongoose.disconnect();
-        reject(err);
-      });
-    });
-  });
+	return new Promise((resolve, reject) => {
+		mongoose.connect(databaseUrl, err => {
+			if (err) {
+				return reject(err);
+			}
+			server = app.listen(port, () => {
+					console.log(`Your app is listening on port ${port}`);
+					resolve();
+				})
+				.on('error', err => {
+					mongoose.disconnect();
+					reject(err);
+				});
+		});
+	});
 }
 
 function closeServer() {
-  return mongoose.disconnect().then(() => {
-    return new Promise((resolve, reject) => {
-      console.log('Closing server');
-      server.close(err => {
-        if (err) {
-          return reject(err);
-        }
-        resolve();
-      });
-    });
-  });
+	return mongoose.disconnect().then(() => {
+		return new Promise((resolve, reject) => {
+			console.log('Closing server');
+			server.close(err => {
+				if (err) {
+					return reject(err);
+				}
+				resolve();
+			});
+		});
+	});
 }
 
 if (require.main === module) {
-  runServer().catch(err => console.error(err));
+	runServer().catch(err => console.error(err));
 }
 
-module.exports = {app, runServer, closeServer};
+module.exports = {
+	app,
+	runServer,
+	closeServer
+};
 
 // var myPost2 = {
 //   title: 'Global Warming',
@@ -73,5 +89,3 @@ module.exports = {app, runServer, closeServer};
 //   author: 'Al Gore',
 //   created: new Date()
 // };
-
-
